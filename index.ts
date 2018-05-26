c2ccd23a1806405a1924058256a3a9a70e1275f0
Daniele Morosinotto <d.morosinotto@icloud.com>
2018-05-26 15:33:25 +0200

    STEP3 - Validazione tipi complessi: array / oggetti annidati

--- index.ts
+++ index.ts
@@ -5,8 +5,17 @@ import { ok, ko, tap, fetch, optional, decodeToPromise, throwErr } from "./helpe
     const People = t.interface({
         name: t.string,
         surname: optional(t.string),
-        sex: optional(t.union([t.literal("M"), t.literal("F")]))
+        sex: optional(t.union([t.literal("M"), t.literal("F")])),
         // sex: optional(t.keyof({ M: null, F: null })) //modo alternativo
+        dob: optional(t.string), //LE DATE IN JSON SONO STRINGHE ISOFormat
+        programming: optional(t.array(t.string)),
+        address: optional(
+            t.interface({
+                via: t.string,
+                num: t.string,
+                cap: t.number
+            })
+        )
     });
 
     type IPeople = t.TypeOf<typeof People>;
@@ -14,15 +23,15 @@ import { ok, ko, tap, fetch, optional, decodeToPromise, throwErr } from "./helpe
     await ValidateAsync("good/1", People);
     await ValidateAsync("good/2", People);
     await ValidateAsync("good/3", People);
-    //await Validate("good/4", People);
-    //await Validate("good/5", People);
+    await Validate("good/4", People);
+    await Validate("good/5", People);
     await ValidateAsync("bad/1", People);
     await ValidateAsync("bad/2", People);
     await ValidateAsync("bad/-1", People);
     await ValidateAsync("bad/-2", People);
     await ValidateAsync("bad/3", People);
-    //await Validate("bad/4", People);
-    //await Validate("bad/5", People);
+    await Validate("bad/4", People);
+    await Validate("bad/5", People);
 })();
 
 /* HELPER LOCALI PER ESEGUIRE VALIDAZIONE SYNC O ASYNC */
